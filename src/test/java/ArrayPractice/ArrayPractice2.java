20446521e04a36bd888261e486e23ca188df80a3
kaustubhsatpudke-dotcom <kaustubh.satpudke@gmail.com>
2026-03-18 18:13:46 +0530

    added comments

---
+++ src/test/java/ArrayPractice/ArrayPractice2.java
@@ -0,0 +1,12 @@
+package ArrayPractice;
+
+public class ArrayPractice2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//This is for practicing git and github.
+		
+		//Additional comment for Kaustub_Programs_Java_Selenium_Test1 Branch
+	}
+
+}
